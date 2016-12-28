a05fb5a04a1070bf32e47886700ea13355867e51
saba <saba_tsertsvadze@hotmail.fr>
2016-12-28 12:59:03 +0100

    added plugins

--- src/main/java/com/erecrutement/Controllers/SignupController.java
+++ src/main/java/com/erecrutement/Controllers/SignupController.java
@@ -3,17 +3,11 @@ package com.erecrutement.Controllers;
 import com.erecrutement.Entities.Candidat;
 import com.erecrutement.Entities.Entreprise;
 import com.erecrutement.Entities.Role;
-import com.erecrutement.Entities.User;
-import com.erecrutement.Repositories.CandidatRepository;
 import com.erecrutement.Repositories.UserRepository;
 import com.erecrutement.Validators.UserValidator;
-import com.erecrutement.ViewModels.CandidatSignupForm;
-import org.hibernate.pretty.MessageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
