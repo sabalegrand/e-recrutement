25ef0255232ae5f351d15bb743cebeb0b1246f63
Hind Elhathout <elhathouthind04@gmail.com>
2017-01-07 10:52:17 +0000

    message

---
+++ src/main/resources/static/plugins/metier.js
@@ -0,0 +1,10 @@
+/**
+ * Created by nunna on 07/01/17.
+ */
+const metiers = {
+    "M1" : "metier1",
+    "M2" : "metier2",
+    "M3" : "metier3",
+    "M4" : "metier4",
+    "M5" : "metier5"
+}
---
+++ src/main/resources/static/plugins/type.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nunna on 07/01/17.
+ */
+const types ={
+    "CDI":"CDI",
+    "CDD":"CDD",
+    "PFE":"PFE",
+    "FREELANCE":"FREELANCE"
+}
